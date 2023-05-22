682f549e738a39bc0298281c567c0b09e60f9ffc
pperamak <pperamak@gmail.com>
2023-05-22 17:45:57 +0300

    tuotantoversio

--- phonebe/index.js
+++ phonebe/index.js
@@ -12,6 +12,7 @@ app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+
 let persons=[
     { 
         "name": "Arto Hellas", 
@@ -38,36 +39,45 @@ let persons=[
 
 
 app.get('/info', (req, res) => {
+  Person.find({}).then(persons=>{
     res.send(`<p>Phonebook has info for ${persons.length} persons</p>
     <p> ${new Date().toString()} </p>`)
+  })
   })
   
   app.get('/api/persons', (req, res) => {
     Person.find({}).then(persons=>{
+      //console.log(persons)
       res.json(persons)
     })
     
   })
 
-app.get('/api/persons/:id', (req, res)=>{
-  const id= Number(req.params.id)
-  const person=persons.find(person => person.id ===id)
+app.get('/api/persons/:id', (req, res, next)=>{
+  
+  Person.findById(req.params.id)
+  .then(person=>{
+  
   if (person){
     res.json(person)
   }else{
     res.status(404).end()
   }
 })
-
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end()
+.catch(error=>next(error))
 })
 
-app.post('/api/persons', (req, res) => {
-  const newId=Math.floor(Math.random()*9999)
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+  .then(result =>{
+   res.status(204).end() 
+  })
+  .catch(error=> next(error))
+  
+})
+
+app.post('/api/persons', (req, res, next) => {
+  //const newId=Math.floor(Math.random()*9999)
   const body=req.body
   if ((!body.name) || (!body.number)){
     return res.status(400).json({ 
@@ -81,12 +91,55 @@ if (persons.some(e=>e.name===body.name)){
   })
 }
 
-  const person=req.body
-  person.id=newId
-  persons=persons.concat(person)
-  res.json(person)
+  //const person=req.body
+  //person.id=newId
+  //persons=persons.concat(person)
+  //res.json(person)
+  const person= new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then(savedPerson=>{
+    res.json(savedPerson)
+  })
+  .catch(error=>next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next)=>{
+  const body=req.body
+
+  const person={
+    name:body.name,
+    number:body.number,
+  }
+
+  Person.findByIdAndUpdate(
+    req.params.id, 
+    person, 
+    {new: true, runValidators: true, context: 'query'})
+    .then(updatedPerson=>{
+      res.json(updatedPerson)
+    })
+    .catch(error=>next(error))
 })
   
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).json({ error: 'malformatted id' });
+  }
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler)
+
   const PORT = process.env.PORT 
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
